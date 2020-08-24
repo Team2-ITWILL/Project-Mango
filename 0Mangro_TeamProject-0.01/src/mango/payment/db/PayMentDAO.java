cc4e6e74e9d720d96d0a8c206fb6554c8a888984
ITWILL <ITWILL@DESKTOP-KJ97GP7>
2020-08-24 19:37:10 +0900

    PayMentDAO작업 중..

--- 0Mangro_TeamProject-0.01/src/mango/payment/db/PayMentDAO.java
+++ 0Mangro_TeamProject-0.01/src/mango/payment/db/PayMentDAO.java
@@ -12,10 +12,44 @@ public class PayMentDAO extends DBconnection implements IPayMent{
 	@Override
 	public int InsertPayment(PaymentBean pb) {
 	
+	 int num=0;
+	 int check=0;
+		
 		try {
 			getConnection();
 			
 			
+			sql="select pm_use_num from payment where mem_email = ?";
+							
+			pstmt=con.prepareStatement(sql);
+			
+			
+			pstmt.setString(1,pb.getMemEmail());
+			
+			
+			rs=pstmt.executeQuery();
+			
+			if(rs.next()){
+				
+				num=rs.getInt(1)+1;
+				
+			}else{
+				
+				num=1;
+				
+			}
+			
+			sql="insert into payment "
+					+ " values(?,?,?,now(),DATE_ADD(NOW(), INTERVAL ? DAY)";
+			
+			pstmt=con.prepareStatement(sql);
+			
+			
+			pstmt.setString(1,pb.getMemEmail());
+			pstmt.setInt(2,num);
+			pstmt.setString(3,pb.getPmName());
+			
+			check=pstmt.executeUpdate();
 			
 			
 			
