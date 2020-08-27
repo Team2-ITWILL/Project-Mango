9b75ac5cd362dbc79bc1517180fe856247bdc9b3
VitoJeong <ckdghk987@gmail.com>
2020-08-27 16:54:26 +0900

    likeAcademy_first

---
+++ 0Mangro_TeamProject-0.01/src/mango/liked_academy/db/LikedAcademyDAO.java
@@ -0,0 +1,90 @@
+package mango.liked_academy.db;
+
+import mango.connection.db.DBconnection;
+
+public class LikedAcademyDAO extends DBconnection{
+	
+	// 학원좋아요 여부확인
+	public int checkLikeAca(LikedAcademyBean bean){
+		
+		int check = 0;
+		
+		try {
+			getConnection();
+			sql = "select * from liked_academy where mem_email = ? and aca_num = ?";
+			
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, bean.getMemEmail());
+			pstmt.setInt(2, bean.getAcaNum());
+			rs = pstmt.executeQuery();
+			
+			// 좋아요가 눌러져있다면 1 반환
+			if(rs.next()){
+				check = 1;
+			}
+			
+			
+		} catch (Exception e) {
+			System.out.println("checkLikeAca()에서 예외 발생 ㅋ.ㅋ");
+			e.printStackTrace();
+		} finally {
+			resourceClose();
+		}
+		
+		return check;
+		
+	} // checkLikeAca() 끝
+	
+	// 학원좋아요
+	public void likeAca(LikedAcademyBean bean){
+		
+		try {
+			getConnection();
+			sql = "insert into liked_academy values (?, ? , ?, now())";
+			
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, bean.getMemEmail());
+			pstmt.setInt(2, bean.getAcaNum());
+			pstmt.setString(3, bean.getAcaName());
+			
+			pstmt.executeUpdate();
+			
+		} catch (Exception e) {
+			System.out.println("likeAca()에서 예외 발생 ㅋ.ㅋ");
+			e.printStackTrace();
+		} finally {
+			resourceClose();
+		}
+		
+	} // likeAca() 끝
+	
+	// 학원좋아요 취소
+	public void unlikeAca(LikedAcademyBean bean){
+		
+		try {
+			getConnection();
+			sql = "delete from liked_academy where mem_email = ? and aca_num = ?";
+			
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, bean.getMemEmail());
+			pstmt.setInt(2, bean.getAcaNum());
+			
+			pstmt.executeUpdate();
+			
+		} catch (Exception e) {
+			System.out.println("unlikeAca()에서 예외 발생 ㅋ.ㅋ");
+			e.printStackTrace();
+		} finally {
+			resourceClose();
+		}
+		
+		
+	} // unlikeAca() 끝
+	
+	
+	
+} // LikedAcademyDAO class 끝
+
+
+
+
