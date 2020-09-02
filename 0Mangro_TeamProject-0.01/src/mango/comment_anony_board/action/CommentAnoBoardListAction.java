206cd9a1073ddd18da4e761b397d939291876ca1
strglass91 <63148617+strglass91@users.noreply.github.com>
2020-09-02 14:21:27 +0900

    20-09-02(수) 14:21 익명댓글관련 add안되는 파일 직접 추가

---
+++ 0Mangro_TeamProject-0.01/src/mango/comment_anony_board/action/CommentAnoBoardListAction.java
@@ -0,0 +1,16 @@
+package mango.comment_anony_board.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import mango.action.Action;
+import mango.action.ActionForward;
+
+public class CommentAnoBoardListAction implements Action {
+
+	@Override
+	public ActionForward excute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		return null;
+	}
+
+}
