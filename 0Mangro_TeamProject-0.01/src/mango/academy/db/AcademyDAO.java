d88d253e147a6e7ad626c7c28e73dfacb36be2f2
ITWILL <ITWILL@DESKTOP-KJ97GP7>
2020-09-07 17:54:04 +0900

    키워드 작업중 결제 페이지 수정

--- 0Mangro_TeamProject-0.01/src/mango/academy/db/AcademyDAO.java
+++ 0Mangro_TeamProject-0.01/src/mango/academy/db/AcademyDAO.java
@@ -1520,12 +1520,7 @@ public class AcademyDAO extends DBconnection implements IAcademy{
 		}
 			
 			while(rs.next()){
-				AcademyBean bean = new AcademyBean(
-						/*rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), 
-						rs.getString(6), rs.getString(7), rs.getString(8), rs.getString(9), rs.getString(10), 
-						rs.getString(11), rs.getString(12), rs.getString(13), rs.getString(14), rs.getString(15),
-						rs.getDouble(17));	*/	
-						
+				AcademyBean bean = new AcademyBean(	
 						rs.getInt(1), rs.getString(2), rs.getString(3), 
 						rs.getString(4), rs.getString(5), rs.getString(6),
 						rs.getString(7), rs.getString(8), rs.getString(9), 
