bff8cdfc0fa46822e6eae4926f97b0f60c48d299
strglass91 <strong-glass91@naver.com>
2020-09-12 23:32:08 +0900

    20-09-12 23:31 - 마이페이지 링크 수정다시,익명관련파일 주석정리

--- 0Mangro_TeamProject-0.01/src/mango/anony_board/action/ReportAnonyBoardAction.java
+++ 0Mangro_TeamProject-0.01/src/mango/anony_board/action/ReportAnonyBoardAction.java
@@ -1,11 +1,7 @@
 package mango.anony_board.action;
 
-import java.io.PrintWriter;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import com.mysql.fabric.xmlrpc.base.Member;
 
 import mango.action.Action;
 import mango.action.ActionForward;
@@ -15,25 +11,22 @@ public class ReportAnonyBoardAction implements Action {
 
 	@Override
 	public ActionForward excute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		System.out.println("AdminAnoCheckAlreadyBannedAction excute()");
+		System.out.println("ReportAnonyBoardAction excute()");
 		
 		request.setCharacterEncoding("UTF-8");
 		
 		int ano_board_num = Integer.parseInt(request.getParameter("ano_board_num"));
 		String ano_board_reporter = request.getParameter("ano_board_reporter");
-		System.out.println("멤버이메일 받아오는지 체크"+ano_board_reporter);
 		String ano_board_reason = request.getParameter("ano_board_reason");
 		
 		AnonyBoardDAO andao = new AnonyBoardDAO();
 		
 		int checkReportDone = andao.reportANBoard(ano_board_reporter,ano_board_reason,ano_board_num);
 		
-		//request.setAttribute("checkIfBanned", checkIfBanned);
-		
-		System.out.println("ano_single 신고를 위한 액션페이지 왔다감");
-		
-		
+		// 값 세팅하기
 		request.setAttribute("checkReportDone", checkReportDone);
+		
+		
 		ActionForward forward = new ActionForward();
 		forward.setRedirect(false);
 		forward.setPath("./AnonyBoardListAction.anob");
